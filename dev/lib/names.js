ef45d8fea3ab9da6faaa8b9dd4c64de5d0a96c86
Colum Bennett <colum.bennett@feedhenry.com>
2016-01-24 14:10:25 +0000

    added gitignore

--- dev/lib/names.js
+++ dev/lib/names.js
@@ -1,22 +1,22 @@
 module.exports = function () {
-    return [
-        'Bruce Wayne',
-        'Wally West',
-        'John Jones',
-        'Kyle Rayner',
-        'Arthur Curry',
-        'Clark Kent',
-        'Barry Allen',
-        'Hal Jordan',
-        'Kara Kent',
-        'Diana Prince',
-        'Ray Palmer',
-        'Oliver Queen',
-        'Barry Allen',
-        'Hal Jordan',
-        'Kara Kent',
-        'Diana Prince',
-        'Ray Palmer',
-        'Oliver Queen'
-    ]
+  return [
+    'Bruce Wayne',
+    'Wally West',
+    'John Jones',
+    'Kyle Rayner',
+    'Arthur Curry',
+    'Clark Kent',
+    'Barry Allen',
+    'Hal Jordan',
+    'Kara Kent',
+    'Diana Prince',
+    'Ray Palmer',
+    'Oliver Queen',
+    'Barry Allen',
+    'Hal Jordan',
+    'Kara Kent',
+    'Diana Prince',
+    'Ray Palmer',
+    'Oliver Queen'
+  ];
 }
